c083ce9300c24bf6c48fb1babda43d40ca2a20d4
王鹏 <wang_15207770071@163.com>
2018-04-09 13:43:34 +0800

    创建项目

---
+++ WPJenkinsDemo/WPJenkinsDemoTests/WPJenkinsDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  WPJenkinsDemoTests.m
+//  WPJenkinsDemoTests
+//
+//  Created by 王鹏 on 2018/4/9.
+//  Copyright © 2018年 王鹏. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WPJenkinsDemoTests : XCTestCase
+
+@end
+
+@implementation WPJenkinsDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
